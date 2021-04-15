263d91f41b324fd2dad5b13f92eebfacd0a93171
Ramirez <mariayeseniar@gmail.com>
2021-04-15 17:21:24 -0400

    uso de js

--- js/index.js
+++ js/index.js
@@ -3,5 +3,5 @@ document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     let nombre = document.querySelector("#nombre-txt").value;
 
     //console.log("Wena shoro", nombre);
-    alert("Wena shoro" + nombre);
+    alert("Wena shoro " + nombre);
 });
